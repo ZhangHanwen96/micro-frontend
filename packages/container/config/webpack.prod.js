03e19c4ef6290098149356d8f28ff29ff3de31be
ZhangHanwen96 <zhw19960308@outlook.com>
2022-02-20 22:49:55 +0800

    add container workflows

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,27 +1,26 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
 const commonConfig = require("./webpack.common");
 
 const pkgJson = require("../package.json");
 
-const devConfig = {
-	mode: "development",
-	devServer: {
-		port: 8080,
-		historyApiFallback: {
-			index: "index.html",
-		},
+const domain = process.env.PRODUCTION_DOMAIN || "http://localhost";
+
+const prodConfig = {
+	mode: "production",
+	output: {
+		filename: "[name].[contenthash].js",
 	},
 	plugins: [
 		new ModuleFederationPlugin({
 			name: "container",
 			remotes: {
-				marketing: "marketing@http://localhost:8081/remoteEntry.js",
+				marketing: `marketing@${domain}/marketing/remoteEntry.js`,
 			},
 			shared: pkgJson.dependencies,
-			// shared: ["react", "react-dom"],
 		}),
 	],
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
